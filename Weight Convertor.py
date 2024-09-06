645bf0d229daf8481c94f18c51081e3db1690df9
veerabhadrayyabelur <veerabhadrayyabelur@gmail.com>
2024-09-06 09:55:23 +0530

    first commit

--- Weight Convertor.py
+++ Weight Convertor.py
@@ -4,13 +4,15 @@ unit = input("Enter the Unit  (K or L) :")
 if unit == "K":
     weight = weight * 2.205
     unit = "lbs"
+    print(f" your Weight is: {round(weight,1)} {unit}")
 elif unit == "L":
     weight = weight / 2.205
     unit = "kgs"
+    print(f" your Weight is: {round(weight,1)} {unit}")
 
 else:
     print("{unit} is invalid")
 
 
-print(f" your Weight is: {weight} {unit}")
+ 
     
